aa39f5a2906462a241b0d76c361c64359e715b3d
lucas ramos <lucasramos1418@gmail.com>
2024-11-15 20:07:30 -0300

    feat: finalizando o gerenciado de senhas

--- model/password.py
+++ model/password.py
@@ -1,25 +1,42 @@
 from datetime import datetime
 from pathlib import Path
 
+
 class BaseModel:
     BASE_DIR = Path(__file__).resolve().parent.parent
     DB_DIR = BASE_DIR / "db"
-    
+
     def save(self):
-        table_path = Path(self.BASE_DIR / f"{self.__class__.__name__}.txt")
-        
+        table_path = Path(self.DB_DIR / f"{self.__class__.__name__}.txt")
+
         if not table_path.exists():
             table_path.touch()
-         
-        with open(table_path, "a") as arq:
+
+        with open(table_path, "+a") as arq:
             arq.write("|".join(list(map(str, self.__dict__.values()))))
             arq.write("\n")
             
+    @classmethod       
+    def get(cls):
+        table_path = Path(cls.DB_DIR / f"{cls.__name__}.txt")
+
+        if not table_path.exists():
+            table_path.touch()
+        
+        with open(table_path, "+r") as arq:
+            r_password = arq.readlines()
+            
+        results = []
+        Attributes = vars(cls())
+        for read in r_password:
+            split_v = read.split('|')
+            tmp_dict = dict(zip(Attributes, split_v))
+            results.append(tmp_dict)
+        
+        return results
+
 class Password(BaseModel):
     def __init__(self, domain=None, password=None, expire=False):
         self.domain = domain
         self.password = password
         self.create_at = datetime.now().isoformat()
-        
-p1 = Password(domain="lucas.com", password="1234")
-p1.save()
--- views/password_views.py
+++ views/password_views.py
@@ -4,25 +4,26 @@ import base64
 from pathlib import Path
 from cryptography.fernet import Fernet, InvalidToken
 
+
 class FernetHasher:
     RANDOM_STRING_CHAR = string.ascii_lowercase + string.ascii_uppercase
     BASE_DIR = Path(__file__).resolve().parent.parent
     KEY_DIR = BASE_DIR / "Keys"
-    
+
     def __init__(self, key):
         if not isinstance(key, bytes):
             key = key.encode()
-            
+
         self.fernet = Fernet(key)
-    
+
     @classmethod
     def _get_random_string(cls, length=25):
         string = ""
         for i in range(length):
             string = string + secrets.choice(cls.RANDOM_STRING_CHAR)
-        
+
         return string
-    
+
     @classmethod
     def create_key(cls, archive=False) -> None:
         value = cls._get_random_string()
@@ -31,22 +32,22 @@ class FernetHasher:
         if archive:
             return key, cls.archive_key(key)
         return key, None
-    
+
     @classmethod
     def archive_key(cls, key):
         file = "key.key"
         while Path(cls.KEY_DIR / file).exists():
             file = f"key_{cls._get_random_string(length=5)}.key"
-            
+
         with open(cls.KEY_DIR / file, "wb") as arq:
             arq.write(key)
 
         return cls.KEY_DIR / file
-    
+
     def encrypt(self, value):
         if not isinstance(value, bytes):
             value = value.encode()
-            
+
         return self.fernet.encrypt(value)
 
     def decrypt(self, value):
@@ -56,6 +57,10 @@ class FernetHasher:
             return self.fernet.decrypt(value).decode()
         except InvalidToken as e:
             return "Token Invalido"
-        
+
 fernet_lucas = FernetHasher("1jLiQtzaAMFGpsIfl6PZLrn1f85YOO0OqWvTfma5SOM=")
-print(fernet_lucas.decrypt("gAAAAABnN5eQDKJunOU_E2qkx4wXw1I_mR2X3KW8HKXJTyxeWWyZ6Yfa0ffnM_DJzvxVy53c8CG63MCCxeLbyil3mK4w91rlCg=="))
+print(
+    fernet_lucas.decrypt(
+        "gAAAAABnN5eQDKJunOU_E2qkx4wXw1I_mR2X3KW8HKXJTyxeWWyZ6Yfa0ffnM_DJzvxVy53c8CG63MCCxeLbyil3mK4w91rlCg=="
+    )
+)
